1073a7a257a232c57e3543be8b81deb103fcc55c
Madhan <rmadhankv3@gmail.com>
2018-09-02 12:21:00 +0530

    updating settings.py

--- DefineMe/settings.py
+++ DefineMe/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
